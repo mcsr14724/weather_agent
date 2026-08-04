086ad7cdb20adcb4420d0f2d916fc54ce18a7de6
mcsr14724 <chandumaddasani145@gmail.com>
2026-08-04 19:31:16 +0530

    added gemini api key to env&config

--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 class Config:
     OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
---
+++ app/graph/llm.py
@@ -0,0 +1,6 @@
+from langchain.chat_models import init_chat_model
+
+
+llm=init_chat_model(
+    model="gemma-4-31b-it"
+)
