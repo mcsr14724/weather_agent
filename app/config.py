8f849018b384e9c23cc5b00db1be2045145c3936
mcsr14724 <chandumaddasani145@gmail.com>
2026-08-05 12:26:53 +0530

    added mongodb uri

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ load_dotenv()
 class Config:
     OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    MONGODB_URI = os.getenv("MONGODB_URI")
 
 
 if __name__ == "__main__":
@@ -20,4 +21,8 @@ if __name__ == "__main__":
 
     print(
         f"GEMINI_API_KEY       : {'Loaded' if Config.GEMINI_API_KEY else 'Missing'}"
+    )
+
+    print(
+        f"MONGODB_URI          : {'Loaded' if Config.MONGODB_URI else 'Missing'}"
     )
