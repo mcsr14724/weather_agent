bb24459e3b40a4f234a29ec97dab16725be07956
mcsr14724 <chandumaddasani145@gmail.com>
2026-08-05 10:30:35 +0530

    graph.py is implemented

--- app/graph/graph.py
+++ app/graph/graph.py
@@ -1,3 +1,35 @@
 from langgraph.graph import StateGraph,START,END
+from app.graph.state import state
+from app.graph.tools import weather_tool
+from langgraph.prebuilt import ToolNode,tools_condition
+from app.graph.llm import chatbot
 
-graph_builder=StateGraph()
+graph_builder=StateGraph(state_schema=state)
+
+tools_node=ToolNode([weather_tool])
+graph_builder.add_node(node="chatbot",action=chatbot)
+graph_builder.add_node(node="tools",action=tools_node)
+
+graph_builder.add_edge(start_key=START,end_key="chatbot")
+graph_builder.add_conditional_edges(source="chatbot",path=tools_condition)
+graph_builder.add_edge(start_key="tools",end_key="chatbot")
+
+graph=graph_builder.compile()
+
+if __name__ == "__main__":
+    from langchain_core.messages import HumanMessage
+
+    response = graph.invoke(
+        {
+            "messages": [
+                HumanMessage(content="What is the weather in Karalapadu?")
+            ]
+        }
+    )
+
+    print("\nConversation:\n")
+
+    for message in response["messages"]:
+        print("=" * 80)
+        print(type(message).__name__)
+        print(message)
