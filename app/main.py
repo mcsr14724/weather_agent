6109bfcd0552a1c090d88e901f88c64b45feb5a5
mcsr14724 <chandumaddasani145@gmail.com>
2026-08-05 11:53:44 +0530

    main.py is implemented

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
 from fastapi import FastAPI
 
 from app.routes.chat import router
+
+app=FastAPI(
+    title="Weather Agent",
+    description="AI-powered weather assistant built with FastAPI and LangGraph.",
+    version="1.0.0"
+)
+
+app.include_router(router=router)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Weather Agent is running"
+    }
+
+if __name__=="__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "app.main:app",
+        reload=True
+    )
