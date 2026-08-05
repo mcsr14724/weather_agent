40f55a30b18a7c2a88a988bbee33d1aa2759e835
mcsr14724 <chandumaddasani145@gmail.com>
2026-08-05 11:41:35 +0530

    chat routes is implemented

---
+++ app/main.py
@@ -0,0 +1,3 @@
+from fastapi import FastAPI
+
+from app.routes.chat import router
---
+++ app/routes/chat.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, HTTPException
+from app.schemas.chat import ChatRequest,ChatResponse
+from app.graph.graph import graph
+from langchain_core.messages import HumanMessage
+from app.utils.extract_response import extract_response
+
+router=APIRouter(prefix="/chat",tags=["chat"])
+
+@router.post("/", response_model=ChatResponse)
+def chat(request: ChatRequest):
+    try:
+        result = graph.invoke(
+            {
+                "messages": [
+                    HumanMessage(content=request.message)
+                ]
+            }
+        )
+
+        return ChatResponse(
+            response=extract_response(result["messages"][-1])
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
+
+if __name__ == "__main__":
+    request = ChatRequest(
+        message="What is the weather in Karalapadu?"
+    )
+
+    response = chat(request)
+
+    print(response.response)
